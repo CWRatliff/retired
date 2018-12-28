7781c0c2c18318ec59449091428b1117cc7c2831
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2018-12-28 11:57:01 -0800

    Add files via upload

--- websurface.py
+++ websurface.py
@@ -4,7 +4,7 @@ import sys
 import time
 import motor_driver
 
-IP_ADDRESS = '192.168.1.x'
+IP_ADDRESS = '192.168.1.10'
 
 steer = 0
 speed = 0
@@ -15,39 +15,46 @@ robot = motor_driver.motor_driver()
 
 @route('/')
 def index():
-	cmd = request.GET.get('command', '')
-		if cmd == 'f':
-            if speed <= 90:
-				speed += 10
-				robot.motor(speed, steer)
+    global steer
+    global speed
+    global left_limit
+    global right_limit
+    
+    cmd = request.GET.get('command', '')
+    if cmd == 'f':
+        if speed <= 90:
+            speed += 10
+            robot.motor(speed, steer)
 
-        elif cmd == 'b':
-            if speed >= -90:
-                speed -= 10
-                robot.motor(speed, steer)
+    elif cmd == 'b':
+        if speed >= -90:
+            speed -= 10
+            robot.motor(speed, steer)
 
-        elif cmd == 'r':
-            if steer > left_limit:
-                steer -= 2
-#                    print("steer = "+str(steer))
-                robot.motor(speed, steer)
+    elif cmd == 'r':
+        if steer > left_limit:
+            steer -= 2
+#               print("steer = "+str(steer))
+            robot.motor(speed, steer)
 
-        elif cmd == 'l':
-            if steer < right_limit:
-                steer += 2
-                robot.motor(speed, steer)
+    elif cmd == 'l':
+        if steer < right_limit:
+            steer += 2
+            robot.motor(speed, steer)
 
 
-#            elif event.key == K_SPACE:
-#				speed = 0
-#                robot.motor(speed, steer)
-		else:
-			print('cmd = '+cmd)
+    elif cmd == 's':
+        speed = 0
+        robot.motor(speed, steer)
+        
+    else:
+        print('cmd = '+cmd)
+    return template('home.tpl')
 
 try:
-	run(host=IP_ADDRESS, port=80)
+    run(host=IP_ADDRESS, port=80)
 finally:
-	print('finally')
+    print('finally')
 
 ''''
             elif event.key == K_RETURN:
