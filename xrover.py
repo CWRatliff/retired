23c5c51249a898805331b4136b3aec1431a43f2d
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-01-15 14:02:14 -0800

    Add files via upload

---
+++ xrover.py
@@ -0,0 +1,65 @@
+#PlayStation interface via Xbee radios
+import sys
+import time
+import smbus
+import motor_driver
+
+addr = 0x08
+bus = smbus.SMBus(1)
+steer = 0
+speed = 0
+accel = 1
+left_limit = -35
+right_limit = 35
+
+#robot = motor_driver.motor_driver()
+
+while True:
+    chr = int(bus.read_byte(addr))
+    if (chr >= 65) and (chr <= 90):
+        print(chr)
+
+        if chr == 81:			#Q
+            sys.exit()
+        if chr == 'F':
+             if speed <= 90:
+                speed += (10 * accel)
+                accel = 1
+#                robot.motor(speed, steer)
+
+        elif chr == 66:                 #B
+            if speed >= -90:
+                speed -= (10 * accel)
+                accel = 1
+#                robot.motor(speed, steer)
+
+        elif chr == 82:                 #R
+            if steer > left_limit:
+                steer -= (2 * accel)
+                accel = 1
+#                robot.motor(speed, steer)
+
+        elif chr == 76:                 #L
+            if steer < right_limit:
+                steer += (2 * accel)
+                accel = 1
+#                robot.motor(speed, steer)
+
+        elif chr == 83:                 #S
+            speed = 0
+#            robot.motor(speed, steer)
+
+        elif chr == 90:                 #Z
+            dt = 2
+            if steer > 0:
+                dt = -2
+                while abs(steer) > 2:
+                    steer += dt
+                    robot.motor(speed, steer)
+                    time.sleep(0.1)
+            steer = 0
+#           robot.motor(speed, steer)
+
+#        elif chr == 'X':
+#           robot.stop_all()
+        print("Motor speed, steer "+str(speed)+", "+str(steer))			
