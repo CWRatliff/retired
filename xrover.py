61dc89eb3714df92a4ff4d6ea43f51e03fd67a1b
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-04-22 13:11:00 -0700

    Add files via upload

--- xrover.py
+++ xrover.py
@@ -12,7 +12,7 @@ accel = 1
 left_limit = -35
 right_limit = 35
 
-#robot = motor_driver.motor_driver()
+robot = motor_driver.motor_driver()
 
 while True:
     chr = int(bus.read_byte(addr))
@@ -21,47 +21,51 @@ while True:
 
         if chr == 81:			#Q
             sys.exit()
-        if chr == 70:                   #F forward
+        if chr == 70:
              if speed <= 90:
                 speed += (10 * accel)
                 accel = 1
-#                robot.motor(speed, steer)
+                robot.motor(speed, steer)
 
-        elif chr == 66:                 #B back
+        elif chr == 66:                 #B
             if speed >= -90:
                 speed -= (10 * accel)
                 accel = 1
-#                robot.motor(speed, steer)
+                robot.motor(speed, steer)
 
-        elif chr == 76:                 #L left
+        elif chr == 76:                 #L
             if steer > left_limit:
                 steer -= (2 * accel)
                 accel = 1
-#                robot.motor(speed, steer)
+                robot.motor(speed, steer)
 
-        elif chr == 82:                 #R right
+        elif chr == 82:                 #R
             if steer < right_limit:
                 steer += (2 * accel)
                 accel = 1
-#                robot.motor(speed, steer)
+                robot.motor(speed, steer)
 
-        elif chr == 83:                 #S stop
+        elif chr == 83:                 #S
             speed = 0
-#            robot.motor(speed, steer)
+            robot.motor(speed, steer)
 
         elif chr == 90:                 #Z
+            robot.motor_speed()
+            '''
             dt = 2
             if steer > 0:
                 dt = -2
                 while abs(steer) > 2:
                     steer += dt
-#                   robot.motor(speed, steer)
+                    robot.motor(speed, steer)
                     time.sleep(0.1)
             steer = 0
-#           robot.motor(speed, steer)
+            robot.motor(speed, steer)
+            '''
 
-#        elif chr == 'X':
-#           robot.stop_all()
+        elif chr == 88:                 #X
+           robot.stop_all()
+           exit()
         print("Motor speed, steer "+str(speed)+", "+str(steer))
         
         bus.write_byte(addr, ord('{'))
@@ -76,4 +80,3 @@ while True:
         if len(sta) > 3:
             bus.write_byte(addr, ord(sta[3:4]))
         bus.write_byte(addr, ord('}'))
-
