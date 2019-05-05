407b882eac0a8929a2d0312699b432d8a7e5c658
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-05 11:52:33 -0700

    Add files via upload

--- xrover1b.py
+++ xrover1b.py
@@ -12,6 +12,7 @@ accel = 1
 left_limit = -35
 right_limit = 35
 epoch = time.time()
+wstr = ""
 
 robot = motor_driver.motor_driver()
 
@@ -21,60 +22,60 @@ robot = motor_driver.motor_driver()
 #        break
 
 while True:
-    chr = int(bus.read_byte(addr))
-    print (chr)
-    if (chr == 0):
+    xbee = int(bus.read_byte(addr))
+    if (xbee == 0):
         continue
+#    print (xbee)
     while True:
-        if (chr == 123):        #start of cmd
+        if (xbee == 123):        #start of cmd
             wstr = ""
-            chr = int(bus.read_byte(addr))
-            if (chr == 0):
-                continue
-            wstr += str(chr)
-            if (chr == 125):
-                print(wstr)
-                chr = wstr[0]
-                break
+        xbee = int(bus.read_byte(addr))
+        if (xbee == 0):
+            continue
+        wstr += chr(xbee)
+        if (xbee == 125):
+            print(wstr)
+            xchr = ord(wstr[0])
+            break
         else:
-            chr = int(bus.read_byte(addr))
-            if (chr == 0):
+            xchr = int(bus.read_byte(addr))
+            if (xchr == 0):
                 continue
              
                 
-    if (chr == 42):
-#       print("ping")
+    if (xchr == 42):
+       print("ping")
        epoch = time.time()
        
-    if (chr >= 65) and (chr <= 90):
+    if (xchr >= 65) and (xchr <= 90):
 
-        if chr == 81:			#Q
+        if xchr == 81:			#Q
             sys.exit()
-        if chr == 70:
+        if xchr == 70:
              if speed <= 90:
                 speed += (10 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 66:                 #B
+        elif xchr == 66:                 #B
             if speed >= -90:
                 speed -= (10 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 76:                 #L
+        elif xchr == 76:                 #L
             if steer > left_limit:
                 steer -= (2 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 82:                 #R
+        elif xchr == 82:                 #R
             if steer < right_limit:
                 steer += (2 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 71:                #GEE
+        elif xchr == 71:                #GEE
             dt = 1
             if steer < (right_limit - 1):
                 while (steer < right_limit):
@@ -84,7 +85,7 @@ while True:
             steer = right_limit
             robot.motor(speed, steer)
 
-        elif chr == 72:                #HAW
+        elif xchr == 72:                #HAW
             dt = 1
             if steer > (left_limit + 1):
                 while (steer > left_limit):
@@ -94,11 +95,11 @@ while True:
             steer = left_limit
             robot.motor(speed, steer)
 
-        elif chr == 83:                 #S
+        elif xchr == 83:                 #S
             speed = 0
             robot.motor(speed, steer)
 
-        elif chr == 90:                 #Z
+        elif xchr == 90:                 #Z
             dt = 1
             if steer > 0:
                 dt = -1
@@ -109,12 +110,12 @@ while True:
             steer = 0
             robot.motor(speed, steer)
 
-        elif chr == 88:                #X exit Select button
+        elif xchr == 88:                #X exit Select button
             robot.stop_all()
             speed = 0
             exit()
             
-#        elif chr == 79:					#O - orientation esp hdg from arduino
+#        elif xchr == 79:					#O - orientation esp hdg from arduino
 #            eos = wstr.find('}')
 #            hdg = int(wstr[1:eos])
 #            print(hdg)
