0e62ffa4075d688cad65c3431c53822a7d578f6f
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-05 14:49:26 -0700

    Add files via upload

--- xrover2.py
+++ xrover2.py
@@ -4,7 +4,6 @@ import time
 import smbus
 import motor_driver
 
-
 addr = 0x08
 bus = smbus.SMBus(1)
 steer = 0
@@ -13,6 +12,7 @@ accel = 1
 left_limit = -35
 right_limit = 35
 epoch = time.time()
+wstr = ""
 
 robot = motor_driver.motor_driver()
 
@@ -22,53 +22,55 @@ robot = motor_driver.motor_driver()
 #        break
 
 while True:
-    chr = int(bus.read_byte(addr))
-    if (chr == 123):					# '{' -> start of message
-        wstr = ""
-        while True:
-            chr = int(bus.read_byte(addr))
-            wstr += chr;
-            if (chr == 125):			# '}' -> end of message
-                continue
-            if (chr == 123):			# restart
-                wstr = ""
-        chr = wstr[0]					# grab lead character
 
-    if (chr == 42):
-#       print("ping")
-        epoch = time.time()
-        continue
+    while True:
+        xbee = int(bus.read_byte(addr))
+        if (xbee == 0):
+            continue
+        if (xbee == 123):               # '{' - start of cmd
+            wstr = ""
+            continue
+        wstr += chr(xbee)
+        if (xbee == 125):
+            print(wstr)
+            xchr = ord(wstr[0])
+            break
+        #endwhile    
+                
+    if (xchr == 42):
+       print("ping")
+       epoch = time.time()
        
-    if (chr >= 65) and (chr <= 90):
+    if (xchr >= 65) and (xchr <= 90):
 
-        if chr == 81:					#Q - quit
+        if xchr == 81:			#Q
             sys.exit()
-
-        if chr == 70:					#F - forward
+            
+        if xchr == 70:
              if speed <= 90:
                 speed += (10 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 66:                 #B - backup
+        elif xchr == 66:                 #B
             if speed >= -90:
                 speed -= (10 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 76:                 #L - left 2 deg
+        elif xchr == 76:                 #L
             if steer > left_limit:
                 steer -= (2 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 82:                 #R - right 2 deg
+        elif xchr == 82:                 #R
             if steer < right_limit:
                 steer += (2 * accel)
                 accel = 1
                 robot.motor(speed, steer)
 
-        elif chr == 71:                #GEE - hard a starboard
+        elif xchr == 71:                #GEE
             dt = 1
             if steer < (right_limit - 1):
                 while (steer < right_limit):
@@ -78,7 +80,7 @@ while True:
             steer = right_limit
             robot.motor(speed, steer)
 
-        elif chr == 72:                #HAW - hard a port
+        elif xchr == 72:                #HAW
             dt = 1
             if steer > (left_limit + 1):
                 while (steer > left_limit):
@@ -88,11 +90,11 @@ while True:
             steer = left_limit
             robot.motor(speed, steer)
 
-        elif chr == 83:                 #S - stop drive motors
+        elif xchr == 83:                 #S
             speed = 0
             robot.motor(speed, steer)
 
-        elif chr == 90:                 #Z - steer straight ahead
+        elif xchr == 90:                 #Z
             dt = 1
             if steer > 0:
                 dt = -1
@@ -103,33 +105,37 @@ while True:
             steer = 0
             robot.motor(speed, steer)
 
-        elif chr == 88:                #X exit Select button
+        elif xchr == 88:                #X exit Select button
             robot.stop_all()
             speed = 0
             exit()
             
-        elif chr == 79:					#O - orientation esp hdg from arduino
+        elif xchr == 79:					#O - orientation esp hdg from arduino
             eos = wstr.find('}')
             hdg = int(wstr[1:eos])
-            print(hdg)
+            print("heading = " + str(hdg))
 
-        #end if (chr >= 65) and (chr <= 90) =======================
-
-        msg = "{v"+str(speed)+","+str(steer)+","+str(hdg)+""
-        print(msg)
-        i = 0
-        while True:
-            xchr = msg[i]
-            bus.write_byte(addr, ord(xchr))
-            if (xchr == '}'):
-                continue
-
-    epoch = time.time()
+        print("Motor speed, steer "+str(speed)+", "+str(steer))
+        
+        bus.write_byte(addr, ord('{'))
+        bus.write_byte(addr, ord('v'))
+        spd = hex(speed & 0xff)
+        bus.write_byte(addr, ord(spd[2:3]))
+        if len(spd) > 3:
+            bus.write_byte(addr, ord(spd[3:4]))
+        bus.write_byte(addr, ord(','))
+        sta = hex(steer & 0xff)
+        bus.write_byte(addr, ord(sta[2:3]))
+        if len(sta) > 3:
+            bus.write_byte(addr, ord(sta[3:4]))
+        bus.write_byte(addr, ord('}'))
+        epoch = time.time()
+        #end if =======================
 
     if (time.time() > (epoch + 1.1)):
 #        print(time.time(),5)
 #        print(epoch,5)
-#        robot.stop_all()
+        robot.stop_all()
         speed = 0;
         
     # end loop ========================
