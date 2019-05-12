393f9ce269f40edc9556982577747798cfceb4e4
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-12 12:14:31 -0700

    Add files via upload

--- spitest3.ino
+++ spitest3.ino
@@ -1,26 +1,27 @@
 #include <SPI.h>
-char ibuff[50] = {"\{A\}.\{B\}.\{O359\}.\{C\}."};
+char ibuff[] = {"{A}.{B}.{O359}.{C}."};
 char obuff[50];
 volatile int itail = 0;
 volatile int ihead = sizeof(ibuff);
+volatile int otail = 0;
 volatile int prox;
 
 void setup() {
   Serial.begin(115200);
   pinMode(MISO, OUTPUT);
-  SPCR |= _BV(SPE);
+  SPCR |= _BV(SPE);        // set to slave mode
   SPI.attachInterrupt();
   }
 
 
 ISR (SPI_STC_vect) {
   byte c = SPDR;
-  if (itail < ihead)
-    SPDR = ibuff[itail];
+  if (itail < ihead)      // send char if buffer not empty
+    SPDR = ibuff[itail++];
   else
     SPDR = 0;
-  if (c)
-    obuff[itail++] = c;
+  if (c)                  // did we get anything?
+    obuff[otail++] = c;
   else
     prox = true;
   }
@@ -28,7 +29,15 @@ ISR (SPI_STC_vect) {
 void loop() {
   if (prox) {
     Serial.println(obuff);
+    otail = 0;
     prox = false;
-
+    Serial.print("tail ");
+    Serial.print(itail);
+    Serial.print(" head  ");
+    Serial.println(ihead);
+    if (itail >= ihead)
+      itail = 0;
     }
+    if (itail >= ihead)
+      itail = 0;
   }
