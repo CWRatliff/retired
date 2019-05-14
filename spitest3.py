573896dea16ad9dbd1c2c02f9e16ec787cd8862f
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-14 15:17:31 -0700

    Add files via upload

--- spitest3.py
+++ spitest3.py
@@ -9,30 +9,34 @@ spi = spidev.SpiDev()
 rc = spi.open(0, 0)
 spi.max_speed_hz = 125000
 time.sleep(0.0005)
+epoch = time.time()
 
 def spisend(cmd):
     i = 0;
     while True:
-        c = cmd[i]
+        d = cmd[i]
+        c = [ord(d)]
         spi.xfer(c)
-        if (c == '}'):
-            spi.xfer(0)
+        if (d == '}'):
+            spi.xfer(NUL)
             break
-        #endif
-    #endwhile
+            #endif
+        i += 1
+        #endwhile
         
 while True:
     while True:
         c = spi.xfer(NUL)
         if (c == 0):
             break;
-        if (c == '{'):
+        d = chr(c[0])
+        if (d == '{'):
             cbuff = '{'
             continue
-        cbuff += str([c])
-        if (c == '}'):
+        cbuff += d
+        if (d == '}'):
             break;
-    #endwhile
+        #endwhile
     print(cbuff)
     spisend(buff)
     time.sleep(5)
