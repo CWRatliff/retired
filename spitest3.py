4a9f4ebb6df44d3dfbcfe75144e8420f0f75495b
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-15 09:44:38 -0700

    Add files via upload

--- spitest3.py
+++ spitest3.py
@@ -3,31 +3,35 @@ import time
 
 buff="{abcdef}"
 cbuff = ""
+flag = False
 i = 0;
 NUL = [0x00]
 spi = spidev.SpiDev()
 rc = spi.open(0, 0)
 spi.max_speed_hz = 125000
 time.sleep(0.0005)
+c = spi.xfer(NUL)           #flush stale data
 epoch = time.time()
 
+# send a command string to slave
 def spisend(cmd):
     i = 0;
     while True:
         d = cmd[i]
         c = [ord(d)]
         spi.xfer(c)
-        if (d == '}'):
-            spi.xfer(NUL)
+        if (d == '}'):      # end of string
+            spi.xfer(NUL)   # flush last char
             break
             #endif
         i += 1
         #endwhile
         
 while True:
+    # read as many chars from slave as are available
     while True:
         c = spi.xfer(NUL)
-        if (c == 0):
+        if (c[0] == 0):
             break;
         d = chr(c[0])
         if (d == '{'):
@@ -35,9 +39,13 @@ while True:
             continue
         cbuff += d
         if (d == '}'):
+            flag = True
             break;
         #endwhile
-    print(cbuff)
+
+    if (flag):   
+        print(cbuff)
+        flag = False
     spisend(buff)
     time.sleep(5)
 
