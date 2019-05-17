66e00d1537a9febde45123d2cbc6644f0be41680
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-17 11:32:16 -0700

    Add files via upload

--- xrover3.py
+++ xrover3.py
@@ -4,7 +4,7 @@
 
 import sys
 import time
-#import smbus
+import smbus
 import spidev
 #import motor_driver
 
@@ -25,6 +25,7 @@ right_limit = 35
 epoch = time.time()
 wstr = ""
 cbuff = ""
+flag = False
 
 #robot = motor_driver.motor_driver()
 
@@ -33,8 +34,8 @@ cbuff = ""
 def spisend(cmd):
     i = 0
     while True:
-        d = cmd[i]
-        c = [ord(d)]
+        d = cmd[i]                      # d - char
+        c = [ord(d)]                    # c - list containing an int
         spi.xfer(c)
         if (d == '}'):                  #end of string
             spi.xfer(NUL)               # flush last char
@@ -65,41 +66,43 @@ while True:
         #endwhile
 
     if (flag):
+        if (len(cbuff) < 3):
+            continue
         xchr = cbuff[1]    
-        if (xchr == 42):
+        if (xchr == '*'):                   #ping
            print("ping")
            epoch = time.time()
            
         if (xchr >= 'A') and (xchr <= 'Z'):
 
-            if xchr == 81:			#Q
+            if xchr == 'Q':			#Quit (unassigned)
                 sys.exit()
                 
-            if xchr == 70:
+            if xchr == 'F':                     #Forward 10-20% D pad up
                  if speed <= 90:
                     speed += (10 * accel)
                     accel = 1
  #                   robot.motor(speed, steer)
 
-            elif xchr == 66:                 #B
+            elif xchr == 'B':                   #B Reverse 10-20% D pad down
                 if speed >= -90:
                     speed -= (10 * accel)
                     accel = 1
  #                   robot.motor(speed, steer)
 
-            elif xchr == 76:                 #L
+            elif xchr == 'L':                   #Left 2 deg, D pad left
                 if steer > left_limit:
                     steer -= (2 * accel)
                     accel = 1
 #                    robot.motor(speed, steer)
 
-            elif xchr == 82:                 #R
+            elif xchr == 'R':                   #Right 2 deg, D pad right
                 if steer < right_limit:
                     steer += (2 * accel)
                     accel = 1
 #                    robot.motor(speed, steer)
 
-            elif xchr == 71:                #GEE
+            elif xchr == 'G':                   #GEE steer right limit, D pad right + L2
                 dt = 1
                 if steer < (right_limit - 1):
                     while (steer < right_limit):
@@ -109,7 +112,7 @@ while True:
                 steer = right_limit
 #                robot.motor(speed, steer)
 
-            elif xchr == 72:                #HAW
+            elif xchr == 'H':                   #HAW steer left limit, D pad right + L2
                 dt = 1
                 if steer > (left_limit + 1):
                     while (steer > left_limit):
@@ -119,11 +122,11 @@ while True:
                 steer = left_limit
 #                robot.motor(speed, steer)
 
-            elif xchr == 83:                 #S
+            elif xchr == 'S':                   #Stop drive motors L3 left joystick button
                 speed = 0
 #                robot.motor(speed, steer)
 
-            elif xchr == 90:                 #Z
+            elif xchr == 'Z':                   #Zero steering wheels left upper trigger
                 dt = 1
                 if steer > 0:
                     dt = -1
@@ -134,19 +137,19 @@ while True:
                 steer = 0
 #               robot.motor(speed, steer)
 
-            elif xchr == 88:                #X exit Select button
+            elif xchr == 'X':                   #X exit Select button
 #                robot.stop_all()
                 speed = 0
                 exit()
                 
-            elif xchr == 'O':					#O - orientation esp hdg from arduino
+            elif xchr == 'O':			#O - orientation esp hdg from arduino
                 eos = cbuff.find('}')
                 hdg = int(cbuff[2:eos])
                 print("heading = " + str(hdg))
 
-            print("Motor speed, steer "+str(speed)+", "+str(steer))
-            cstr = "{v"+str(speed)+','+str(steer)+','+str(hdg)+'}'
-            spisend(cstr)
+                print("Motor speed, steer "+str(speed)+", "+str(steer))
+                cstr = "{v"+str(speed)+','+str(steer)+','+str(hdg)+'}'
+                spisend(cstr)
 
             epoch = time.time()
             #end if =======================
