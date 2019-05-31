f2399e59990e93b20dd8ec871df8e885483df437
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-05-31 08:02:53 -0700

    Add files via upload

---
+++ RaspardSPIBNO080.ino
@@ -0,0 +1,140 @@
+#include <SPI.h>
+#include <Wire.h>
+#include "SparkFun_BNO080_Arduino_Library.h"
+#include <math.h>
+
+BNO080 myIMU;
+
+unsigned long epoch;
+char ibuffer[256];
+char obuffer[256];
+char str[10];
+volatile int itail = 0;
+volatile int ihead = 0;
+volatile int otail = 0;
+volatile int ohead = 0;
+volatile int prox;
+
+#define MASK 0xff
+
+/* Set the delay between fresh samples */
+#define VARIATION 12.1387                               // for Camarillo, CA
+#define ROVER     0                             // orientation of sensor to robot
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial1.begin(9600);
+  pinMode(MISO, OUTPUT);
+  SPCR |= _BV(SPE);           // set to slave mode
+  SPI.attachInterrupt();
+
+  Wire.begin();
+  myIMU.begin();
+  myIMU.enableRotationVector(450); //Send data update every 450ms
+  }
+//============================================================
+ISR (SPI_STC_vect) {
+  byte c = SPDR;
+  if (c == 0) {               // no char, has to be querry
+    if (itail != ihead) {     // something to xfer
+      SPDR = ibuffer[itail++];  // get FIFO xbee rcvd data
+      itail &= MASK;
+      }
+    }
+  else {
+    obuffer[ohead++] = c;       // save downloaded char
+    ohead &= MASK;
+    }
+  }
+//============================================================
+void loop() {
+  char* p;
+  char  xchr;
+
+  //Look for reports from the IMU
+  if (myIMU.dataAvailable() == true) {
+    float qx = myIMU.getQuatI();
+    float qy = myIMU.getQuatJ();
+    float qz = myIMU.getQuatK();
+    float qw = myIMU.getQuatReal();
+    float quatRadianAccuracy = myIMU.getQuatRadianAccuracy();
+
+    Serial.print(qx, 2);
+    Serial.print(F(","));
+    Serial.print(qy, 2);
+    Serial.print(F(","));
+    Serial.print(qz, 2);
+    Serial.print(F(","));
+    Serial.print(qw, 2);
+    Serial.print(F(","));
+    Serial.print(quatRadianAccuracy, 2);
+    
+    // convert quaternian to Euler angles
+    // roll (x-axis rotation)
+    double sinr_cosp = +2.0 * (qw * qx + qy * qz);
+    double cosr_cosp = +1.0 - 2.0 * (qx * qx + qy * qy);
+    double roll = atan2(sinr_cosp, cosr_cosp);
+  
+    // pitch (y-axis rotation)
+    double sinp = +2.0 * (qw * qy - qz * qx);
+    double pitch;
+    if (fabs(sinp) >= 1)
+      pitch = copysign(M_PI / 2, sinp); // use 90 degrees if out of range
+    else
+      pitch = asin(sinp);
+  
+    // yaw (z-axis rotation)
+    double siny_cosp = +2.0 * (qw * qz + qx * qy);
+    double cosy_cosp  = +1.0 - 2.0 * (qy * qy + qz * qz);  
+    double yaw = atan2(siny_cosp, cosy_cosp) * 180/M_PI;
+  
+  
+      Serial.println();
+      delay(400);
+    // compose 'O'rientation msg for Pi
+    if ((millis() - epoch) > 1000) {
+      // Can Dead Men Vote Twice At Elections
+      int hdg = 360 - yaw;
+      hdg = hdg + VARIATION;            // make Mag heading into True
+      hdg += ROVER;                         // orientation on rover chassis
+      if (hdg < 0)
+        hdg += 360;
+      if (hdg > 360)
+        hdg -= 360;
+      sprintf(str, "{O%d}.", hdg);
+      for (char *p = &str[0]; *p; p++) {
+        ibuffer[ihead++] = *p;
+        ihead &= MASK;
+        }
+  
+      Serial.print("yaw:");
+      Serial.print(hdg);
+      Serial.print(" pitch: ");
+      Serial.print(pitch*180.0/M_PI, 2);
+      Serial.print(" roll: ");
+      Serial.print(roll*180.0/M_PI, 2);
+      Serial.println(str);
+      epoch = millis();
+      }
+    
+    // read Xbee input and upload to Pi
+    while (Serial1.available()) {
+      xchr = Serial1.read();
+      ibuffer[ihead++] = xchr;
+      ihead &= MASK;
+      if (xchr == '}') {
+        ibuffer[ihead++] = 0;
+        ihead &= MASK;
+        }
+      }
+      
+    // if any data waiting in output buffer, transmit via Xbee    
+    if (ohead != otail) {
+      if (Serial1.availableForWrite()) {
+        Serial1.write(obuffer[otail++]);
+        otail &= MASK;
+        }
+      }
+    }
+  } 
