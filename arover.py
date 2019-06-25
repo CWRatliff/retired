ccdfd31d7c638ce2ae45756b8f41305c64e3e5c5
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-06-25 11:04:57 -0700

    Add files via upload

--- arover.py
+++ arover.py
@@ -23,6 +23,7 @@ oldsteer = 500
 oldspeed = 500
 oldhdg = 500
 compass = False
+comhdg = 0
 
 left_limit = -36
 right_limit = 36
@@ -65,147 +66,155 @@ def spisend(cmd):
 #    if (chr == 0):
 #        break
 #=================================================================
-while True:
+try:
+    while True:
 
-    while True:                         # read characters from slave
-        c = spi.xfer(NUL)
-        if (c[0] == 0 or c[0] == 255):
-            break
-#        print(c)
-        d = chr(c[0])
-        if (d == '{'):
-            cbuff = "{"
-            continue
-        cbuff += d
-        if (d == '}'):
-            flag = True
-            break
-        #endwhile
+        while True:                         # read characters from slave
+            c = spi.xfer(NUL)
+            if (c[0] == 0 or c[0] == 255):
+                break
+            print(c)
+            d = chr(c[0])
+            if (d == '{'):
+                cbuff = "{"
+                continue
+            cbuff += d
+            if (d == '}'):
+                flag = True
+                break
+            #endwhile
 
-    if (flag):                          # flag means we got a command
-        if (len(cbuff) < 3):
-            continue
-        xchr = cbuff[1]    
-        if (xchr == '*'):                   #ping
-#           print("ping")
-           epoch = time.time()
-           
-        if (xchr >= 'A') and (xchr <= 'Z'):
+        if (flag):                          # flag means we got a command
+            if (len(cbuff) < 3):
+                continue
+            xchr = cbuff[1]    
+            if (xchr == '*'):                   #ping
+    #           print("ping")
+               epoch = time.time()
+    #        print(xchr)
+               
+            if (xchr >= 'A') and (xchr <= 'Z'):
 
-            if xchr == 'Q':			#Quit (unassigned)
-                sys.exit()
-                
-            if xchr == 'F':                     #Forward 10-20% D pad up
-                 if speed <= 90:
-                    speed += 10
-                    robot.motor(speed, steer)
-
-            elif xchr == 'B':                   #B Reverse 10-20% D pad down
-                if speed >= -90:
-                    speed -= 10
-                    robot.motor(speed, steer)
-
-            elif xchr == 'C':                   #C steer to compass heading
-                compass = True
-
-            elif xchr == 'L':                   #Left 2 deg, D pad left
-                if steer > left_limit:
-                    steer -= turn()
-                    robot.motor(speed, steer)
-
-            elif xchr == 'R':                   #Right 2 deg, D pad right
-                if steer < right_limit:
-                    steer += turn()
-                    robot.motor(speed, steer)
-
-            elif xchr == 'G':                   #GEE steer right limit, D pad right + L2
-                dt = 1
-                if steer < (right_limit - 1):
-                    while (steer < right_limit):
-                        steer += dt
-                        robot.motor(speed, steer)
-    #                    time.sleep(0.05)
-                steer = right_limit
-                robot.motor(speed, steer)
-
-            elif xchr == 'H':                   #HAW steer left limit, D pad right + L2
-                dt = 1
-                if steer > (left_limit + 1):
-                    while (steer > left_limit):
-                        steer -= dt
-                        robot.motor(speed, steer)
-    #                    time.sleep(0.05)
-                steer = left_limit
-                robot.motor(speed, steer)
-
-            elif xchr == 'S':                   #Stop drive motors L3 left joystick button
-                speed = 0
-                robot.motor(speed, steer)
-
-            elif xchr == 'Z':                   #Zero steering wheels left upper trigger
-                dt = 1
-                if steer > 0:
-                    dt = -1
-                    while abs(steer) > 1:
-                        steer += dt
-                        robot.motor(speed, steer)
-    #                    time.sleep(0.05)
-                steer = 0
-                robot.motor(speed, steer)
-                compass = False
-
-            elif xchr == 'X':                   #X exit Select button
-                robot.stop_all()
-                speed = 0
-                exit()
-                
-            elif xchr == 'O':			#O - orientation esp hdg from arduino
-                eos = cbuff.find('}')
-                hdg = int(cbuff[2:eos])
-                print("heading = " + str(hdg))
-
-                print("Motor speed, steer "+str(speed)+", "+str(steer))
-
-            if (compass);
-                if (oldhdg < 180 and hdg > 180):    #seems crude but cheap vs vectors
-                    steer = -2
-                elif (oldhdg > 180 and hdg < 180):
-                    steer = 2
-                elif (oldhdg > hdg):
-                    steer = hdg - oldhdg
-                elif (oldhdg < hdg):
-                    steer = oldhdg - hdg
+                if xchr == 'Q':			#Quit (unassigned)
+                    sys.exit()
                     
-                if (steer != 0):
+                if xchr == 'F':                     #Forward 10-20% D pad up
+                     if speed <= 90:
+                        speed += 10
+                        robot.motor(speed, steer)
+
+                elif xchr == 'B':                   #B Reverse 10-20% D pad down
+                    if speed >= -90:
+                        speed -= 10
+                        robot.motor(speed, steer)
+
+                elif xchr == 'C':                   #C steer to compass heading
+                    compass = True
+                    comhdg = hdg
+
+                elif xchr == 'L':                   #Left 2 deg, D pad left
+                    if steer > left_limit:
+                        steer -= turn()
+                        robot.motor(speed, steer)
+
+                elif xchr == 'R':                   #Right 2 deg, D pad right
+                    if steer < right_limit:
+                        steer += turn()
+                        robot.motor(speed, steer)
+
+                elif xchr == 'G':                   #GEE steer right limit, D pad right + L2
+                    dt = 1
+                    if steer < (right_limit - 1):
+                        while (steer < right_limit):
+                            steer += dt
+                            robot.motor(speed, steer)
+        #                    time.sleep(0.05)
+                    steer = right_limit
                     robot.motor(speed, steer)
+
+                elif xchr == 'H':                   #HAW steer left limit, D pad right + L2
+                    dt = 1
+                    if steer > (left_limit + 1):
+                        while (steer > left_limit):
+                            steer -= dt
+                            robot.motor(speed, steer)
+        #                    time.sleep(0.05)
+                    steer = left_limit
+                    robot.motor(speed, steer)
+
+                elif xchr == 'S':                   #Stop drive motors L3 left joystick button
+                    speed = 0
+                    robot.motor(speed, steer)
+
+                elif xchr == 'Z':                   #Zero steering wheels left upper trigger
+                    dt = 1
+                    if steer > 0:
+                        dt = -1
+                        while abs(steer) > 1:
+                            steer += dt
+                            robot.motor(speed, steer)
+        #                    time.sleep(0.05)
+                    steer = 0
+                    robot.motor(speed, steer)
+                    compass = False
+                    comhdg = 0
+
+                elif xchr == 'X':                   #X exit Select button
+                    robot.stop_all()
+                    speed = 0
+                    exit()
                     
-            if (hdg != oldhdg):
-                cstr = "{h"+str(hdg)+"}"
-                spisend(cstr)
-                oldhdg = hdg
-                print(cstr)
-            if (speed != oldspeed):
-                cstr = "{v"+str(speed)+"}"
-                spisend(cstr)
-                oldspeed = speed
-                print(cstr)
-            if (steer != oldsteer):
-                cstr = "{s"+str(steer)+"}'"
-                spisend(cstr)
-                oldsteer = steer
-                print(cstr)
+                elif xchr == 'O':			#O - orientation esp hdg from arduino
+                    eos = cbuff.find('}')
+                    hdg = int(cbuff[2:eos])
+                    print("heading = " + str(hdg))
 
-            epoch = time.time()
-            #end if =======================
+                    print("Motor speed, steer "+str(speed)+", "+str(steer))
 
-        if (time.time() > (epoch + 1.1)):
-    #        print(time.time(),5)
-    #        print(epoch,5)
-    #        robot.stop_all()
-            speed = 0;
-            
-        # end loop ========================
-    flag = False
-    cbuff = ""
-    #end if flag
+                if (compass):
+                    if (comhdg < 180 and hdg > 180):    #seems crude but cheap vs vectors
+                        steer = 2
+                    elif (comhdg > 180 and hdg < 180):
+                        steer = -2
+                    elif (comhdg > hdg):
+                        steer = 2
+                    elif (comhdg < hdg):
+                        steer = -2
+                        
+                    if (steer != 0):
+                        robot.motor(speed, steer)
+                        
+                if (hdg != oldhdg):
+                    cstr = "{h"+str(hdg)+"}"
+                    spisend(cstr)
+                    oldhdg = hdg
+                    print(cstr)
+                if (speed != oldspeed):
+                    cstr = "{v"+str(speed)+"}"
+                    spisend(cstr)
+                    oldspeed = speed
+                    print(cstr)
+                if (steer != oldsteer):
+                    cstr = "{s"+str(steer)+"}'"
+                    spisend(cstr)
+                    oldsteer = steer
+                    print(cstr)
+
+                epoch = time.time()
+                #end if =======================
+
+            if (time.time() > (epoch + 1.1)):
+        #        print(time.time(),5)
+        #        print(epoch,5)
+        #        robot.stop_all()
+                speed = 0;
+                
+            # end loop ========================
+        flag = False
+        cbuff = ""
+        #end if flag
+
+finally:
+    robot.motor(0,0)
+    print("Halted")
 #    robot.deinit()
