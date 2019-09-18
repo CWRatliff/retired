77938ba7cb38407c47941bcc37bf550f6b701a24
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-09-18 09:01:46 -0700

    Add files via upload

--- EKF.py
+++ EKF.py
@@ -28,18 +28,13 @@ def motion_model(x, u):
                   [0, 1.0, 0, 0],
                   [0, 0, 1.0, 0],
                   [0, 0, 0, 1.0]])
-    B = np.array([[DT * math.cos(u[1]), 0],
-                  [DT * math.sin(u[1]), 0],
+    B = np.array([[DT * math.cos(u[0, 1]), 0],
+                  [DT * math.sin(u[0, 1]), 0],
                   [0.0, DT],
                   [1.0, 0.0]])
-    print("motion B", B)
-    Bu = B.dot(u)
-    print("motion Bu", Bu)
-    But = Bu.T
-    print("motion BuT", But)
-    x = F.dot(x) + B.dot(u)
-#   x = F.dot(x) + Bu.T
-#    x = x.T
+
+    x = F.dot(x) + B.dot(u.T)
+
     print("motion ret x", x)
     return x
     
@@ -49,8 +44,6 @@ def observation_model(x):
     H = np.array([                              # measurement function
         [1, 0, 0, 0],
         [0, 1, 0, 0]])
-    print ("H",H)
-    print("x", x)
     z = H.dot(x)
     print("obs_mod z", z)
     return z
@@ -58,15 +51,13 @@ def observation_model(x):
 #motion model Jacobian matrix
 # x - state vector
 # u - input vector
-def jacobiF(x, u):
-    v = u[0]
+def jacobiF(u):
+    v = u[0, 0]
     d = DT * v
-    alpha = u[1]
+    alpha = u[0, 1]
     sina = math.sin(alpha)
-    print("sina", sina)
     cosa = math.cos(alpha)
     dsina = sina * d
-    print("dsina", dsina)
     dcosa = cosa * d
     jF=np.array([
         [1.0, 0.0, -dsina, DT * cosa],
@@ -76,8 +67,7 @@ def jacobiF(x, u):
     return jF
 
 #observation model Jacobian matrix
-# x - state vector
-def jacobiH(x):
+def jacobiH():
     jH = np.array([
         [1, 0, 0, 0],
         [0, 1, 0, 0]])
@@ -91,31 +81,20 @@ def Kalman_filter(xEst, pEst, z, u):
     # predict
     xPred = motion_model(xEst, u)
     print ("xPred", xPred)
-    jF = jacobiF(xPred, u)
+    jF = jacobiF(u)
     print("jF",jF)
     print("pEst", pEst)
     print("R", R)
-#    pPred = jF.dot(pEst).dot(jF.T) + R
-    pPred = jF.dot(pEst)
-    print("Pred0", pPred)
-    pPred = pPred.dot(jF.T) + R
-    print("Pred1", pPred)
-    pPred = pPred + R
-    print("Pred2", pPred)
+    pPred = jF.dot(pEst).dot(jF.T) + R
+    print("Pred", pPred)
     
     # update
-    jH = jacobiH(xPred)
+    jH = jacobiH()
     zPred = observation_model(xPred)
     print("z", z)
     print("zPred", zPred)
     y = z.T - zPred
-#    S = jH.dot(pPred).dot(jH.T) + Q
-    S = jH.dot(pPred)
-    print("K-F S1", S)
-    S = S.dot(jH.T)
-    print("K-F S2", S)
-    print("K-F Q", Q)
-    S = S + Q
+    S = jH.dot(pPred).dot(jH.T) + Q
     print("K-F S", S)
     K = pPred.dot(jH.T).dot(np.linalg.inv(S))
     xEst = xPred + K.dot(y)
@@ -127,13 +106,14 @@ def Kalman_start(begin):
     xEst = np.zeros((4,1))              # infinite a priori
     pEst = np.zeros((4,1))
 
-# time - hms of sample
+# time - delta T of sample
 # speed - fps
 # hdg - radians (x-axis zero)
-# lonft - x-axis
-# latft - y-axis
+# x - x-axis
+# y - y-axis
 def Kalman_step(DT, speed, hdg, x, y):
-    u = (np.array([[speed, hdg]])).T
+#    u = (np.array([[speed, hdg]])).T
+    u = (np.array([[speed, hdg]]))
     z = np.array([[x, y]])
     nxEst, npEst = Kalman_filter(xEst, pEst, z, u)
     return nxEst, npEst
@@ -148,10 +128,8 @@ for i in range(2):
     spd = test[i][1] * spdfactor
 #    ang = np.deg2rad(450-test[0][2])
     ang = 3.14157/180*(450-test[i][2])
-    x = test[i][3] + lonfeet
+    x = -test[i][3] + lonfeet
     y = test[i][4] * latfeet
-    
-    z = [x, y]
-    u = [spd, ang]
+
     print (spd, ang, x, y)
     xnew = Kalman_step(deltaT, spd, ang, x, y)
