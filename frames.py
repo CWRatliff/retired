d9384a956091ce18803eda6e129715110563243c
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2019-10-18 15:53:39 -0700

    Add files via upload

--- frames.py
+++ frames.py
@@ -57,37 +57,37 @@ class App:
 
         
         keypad=Frame(master)
-        b1=Button(keypad, text="1")
+        b1=Button(keypad, text="1", command=self.one)
         b1.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b1.grid(row=0,column=0)
-        b2=Button(keypad, text="2")
+        b2=Button(keypad, text="2", command=self.two)
         b2.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b2.grid(row=0,column=1)
-        b3=Button(keypad, text="3")
+        b3=Button(keypad, text="3", command=self.three)
         b3.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b3.grid(row=0,column=2)
         bA=Button(keypad, text="A")
         bA.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         bA.grid(row=0,column=3)
-        b4=Button(keypad, text="4")
+        b4=Button(keypad, text="4", command=self.four)
         b4.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b4.grid(row=1,column=0)
-        b5=Button(keypad, text="5")
+        b5=Button(keypad, text="5", command=self.five)
         b5.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b5.grid(row=1,column=1)
-        b6=Button(keypad, text="6")
+        b6=Button(keypad, text="6", command=self.six)
         b6.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b6.grid(row=1,column=2)
         bB=Button(keypad, text="B")
         bB.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         bB.grid(row=1,column=3)
-        b7=Button(keypad, text="7")
+        b7=Button(keypad, text="7", command=self.seven)
         b7.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b7.grid(row=2,column=0)
-        b8=Button(keypad, text="8")
+        b8=Button(keypad, text="8", command=self.eight)
         b8.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b8.grid(row=2,column=1)
-        b9=Button(keypad, text="9")
+        b9=Button(keypad, text="9", command=self.nine)
         b9.config(width=2,height=2,font=(None,25),bg="blue",fg="white")
         b9.grid(row=2,column=2)
         bC=Button(keypad, text="C")
@@ -111,23 +111,77 @@ class App:
 #        keypad.grid(row=0,column=1)
         keypad.place(x=380,y=10)
         
-        self.input = ""
+        self.ibuffer = ""
+        self.msg = ""
         self.exeflag = False
         self.lbflag = False
+        self.lb2flag = False
         self.piflag = False
         
     def star_button(self):
-        exeflag = True
+        self.exeflag = True
         self.status.set("Auto")
         self.steer.set("-16")
         self.xte.set("3.45")
         self.lat.set("20.1255")
         self.lon.set("6.732")
+    def pound(self):
+        self.lbflag = True
+        self.lb2flag = False
+    def zero(self):
+        self.key = '0'
+        self.xmit()
+    def one(self):
+        self.key = '1'
+        self.xmit()
+    def two(self):
+        self.key = '2'
+        self.xmit()
+    def three(self):
+        self.key = '3'
+        self.xmit()
+    def four(self):
+        self.key = '4'
+        self.xmit()
+    def five(self):
+        self.key = '5'
+        self.xmit()
+    def six(self):
+        self.key = '6'
+        self.xmit()
+    def seven(self):
+        self.key = '7'
+        self.xmit()
+    def eight(self):
+        self.key = '8'
+        self.xmit()
+    def nine(self):
+        self.key = '9'
+        self.xmit()
+        
+    def xmit(self):
+        if (self.exeflag):
+            self.msg = '{E' + self.key + '}'
+            ser.write(self.msg.encode('utf-8'))
+            self.exeflag = False
+        elif (self.lbflag):
+            if (self.lb2flag):
+                self.msg = self.msg + self.key + '}'
+                ser.write(msg.encode('utf-8'))
+                self.lbflag = False
+                self.lb2flag = False
+            else:
+                self.msg = '{F' + self.key
+                self.lb2flag = True
+        else:
+            self.msg = '{D' + self.key + '}'
+            ser.write(self.msg.encode('utf-8'))
+        self.msg = ""
 
 #   Listen to serial port for status info from rover pi
     def listen(self):
         while ser.in_waiting:
-            inpt = ser.read(1)
+            inpt = ser.read(1).decode("utf-8")
             print(inpt)
             if (inpt == '{'):
                 self.ihead = 0
@@ -140,13 +194,32 @@ class App:
            
         if self.piflag:
             xchar = self.ibuffer[0]
-            self.ibuffer += self.ibuffer[1:]
-            if (xchar == 'v'):
-                self.speed.set(self.ibuffer)
+            lbuffer = self.ibuffer[1:]
+            
+            if (xchar == 'a'):
+                self.status.set(lbuffer)
                     
-            if (xchar == 'd'):
-                self.dtg.set(self.ibuffer)
+            elif (xchar == 'c'):
+                self.ctg.set(lbuffer)
                     
+            elif (xchar == 'd'):
+                self.dtg.set(lbuffer)
+                    
+            elif (xchar == 'h'):
+                self.head.set(lbuffer)
+                    
+            elif (xchar == 'l'):
+                self.lat.set(lbuffer)
+                    
+            elif (xchar == 'n'):
+                self.lon.set(lbuffer)
+                    
+            elif (xchar == 's'):
+                self.steer.set(lbuffer)
+                    
+            elif (xchar == 'v'):
+                self.speed.set(lbuffer)
+ 
             self.piflag = False
                         
         root.after(25, self.listen)
