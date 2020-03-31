60f1be6c8d658c033e972402c7e355fac8c3ad76
CWRatliff <42357664+CWRatliff@users.noreply.github.com>
2020-03-31 09:57:56 -0700

    Add files via upload

--- testEKF.py
+++ testEKF.py
@@ -1,35 +1,35 @@
-import cEKF
-import numpy as np
+import dEKF
+#import numpy as np
 import math
+import time
 
-spdfactor = .0035
+print("time", time.time())
+spdfactor = .008
 d3 = 10.5
 d1 = 7.254
-lonfeet = 83.716
+lonfeet = -83.716
 latfeet = 101.267
 ang = 0
 test = [
-    [34, 60, -6, 7.082, 20.532],
-    [37, 60, -10, 7.082, 20.545],
-    [43, 60, 1, 7.082, 20.559],
-    [45, 60, 4, 7.138, 20.559],
+    [15, 50, 336, 7.0798, 22.4219],
+    [17, 50, 336, 7.0808, 22.4219],
+    [18, 50, 335, 7.0837, 22.428],
+    [19, 50, 336, 7.0859, 22.4312],
     ]
 
-Kfilter = cEKF.Kalman_filter()
+Kfilter = dEKF.Kalman_filter()
 
-Kfilter.Kalman_start(32, -7.082 * lonfeet, 20.532 * latfeet, -2.96, 60*spdfactor)
+Kfilter.Kalman_start(14, 7.0798 * lonfeet, 22.4190 * latfeet, math.radians(450-336), 50*spdfactor)
 for i in range(4):
     time = test[i][0]
 
     v = test[i][1] * spdfactor
-    steer = test[i][2]
-    alpha = np.deg2rad(steer)
-    h = d3/math.sin(alpha)
-    turnrad = (h * math.cos(alpha) + d1) / 12
-    omega = v / turnrad
-    xl = -test[i][3] * lonfeet
-    yl = test[i][4] * latfeet
+    hdg = test[i][2]
 
-    print (v, omega, xl, yl)
-    xest = Kfilter.Kalman_step(time, xl, yl, omega, v)
-    print("Kalman new est", xest)
+    xl = test[i][3] * lonfeet
+    yl = test[i][4] * latfeet
+    phi = math.radians(450-hdg)
+
+    print (v, phi, xl, yl)
+    xest = Kfilter.Kalman_step(time, xl, yl, phi, v)
+    print("Kalman new est", xest[0]/lonfeet, xest[1]/latfeet, 450-math.degrees(xest[2]), xest[3])
